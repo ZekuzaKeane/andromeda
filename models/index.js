a01766fcd1802c947642e190c38c642cca11959f
nic-d <nicanthonidiaz@gmail.com>
2023-11-29 22:40:55 -0500

    User model created and completed

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Comment = require('./Comment'); // Note: need to update if we adjust this model to something different (i.e: Pins)
+const Constellation = require('./Constellation');
+
+// List associations below 
---
+++ models/user.js
@@ -0,0 +1,71 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4, 20],
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+        signupDate: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isDate: true,
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 12);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 12);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+
+
+
+
+
+
+
+module.exports = User;
