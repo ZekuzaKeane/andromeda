0c19d31e0e44b6da4af597ed6e5847c10b0cc694
nic-d <nicanthonidiaz@gmail.com>
2023-12-05 12:37:51 -0500

    created login / sign up scripts to run whenever login / sign up buttons are submitted. Seems we are intending to only use the username and pass to sign up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,17 +17,17 @@ router.post('/', async (req, res) => {
 
 router.post('./login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const userData = await User.findOne({ where: { username: req.body.username } });
 
         if (!userData) {
-            res.status(400).json({ message: 'The email or password you provided is incorrect, please try again' });
+            res.status(400).json({ message: 'The username or password you provided is incorrect, please try again' });
             return;
         };
 
         const validPassword = await userData.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({ message: 'The email or password you provided is incorrect, please try again' });
+            res.status(400).json({ message: 'The username or password you provided is incorrect, please try again' });
             return;
         }
 
